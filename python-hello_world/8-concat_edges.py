420b550c76ce7998897bd50946b17cd51be81ba0
bryanjosepr <bryan.jose2541@gmail.com>
2025-05-09 12:46:40 -0400

    6 code

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+word = "Python"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(str)
