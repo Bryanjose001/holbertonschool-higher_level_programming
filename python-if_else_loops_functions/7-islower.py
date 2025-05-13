56371e118ff63474ad9079909bee995875f64169
bryanjosepr <bryan.jose2541@gmail.com>
2025-05-13 13:44:42 -0400

    fix

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
-print(islower('a'))  # True
-print(islower('z'))  # True
-print(islower('A'))  # False
-print(islower('!'))  # False
