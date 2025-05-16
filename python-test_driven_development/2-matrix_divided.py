2611af6ace6fb0f8d0005098df8102bde5c1f767
bryanjosepr <bryan.jose2541@gmail.com>
2025-05-16 13:50:54 -0400

    test 1

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Divide all elements of a matrix by a given number and round to 2 decimal
+    places."""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divide all elements of a matrix by a given number and round to 2 decimal
+    places.
+    """
+    error = "matrix must be a matrix (array of arrays of integers/floats)"
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for rows in matrix:
+        if type(rows) is not list:
+            raise TypeError(error)
+        if len(rows) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for index in rows:
+            if type(index) not in (int, float):
+                raise TypeError(error)
+    return ([list(map(lambda x: round(x / div, 2), rows)) for rows in matrix])
