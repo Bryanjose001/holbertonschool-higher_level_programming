3b2a35d8b4fcd06e914b0c650dc9c6407f0e0096
Bryanjose001 <bryan.jose2541@gmail.com>
2025-06-13 16:16:03 -0400

    api

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -11,14 +11,18 @@ def fetch_and_print_posts():
                 print(d['title'])
 def fetch_and_save_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print ('status Code:',r.status_code)
     if r.status_code == HTTPStatus.OK:
         data = r.json()
-        print(data)
-        if data:
-            for d in data:
-                
+        structured_posts = [{'id': post['id'], 'title': post['title'],
+                            'body': post['body']} for post in posts]
 
+        with open('posts.csv', mode='w', newline='',
+                  encoding='utf-8') as csv_file:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-fetch_and_print_posts()
-fetch_and_save_posts()
+            writer.writeheader()
+            writer.writerows(structured_posts)
+        print("Data has benn succesfully written to posts.csv")
+    else:
+        print(f"Failed to fetch posts. Status code: {r.status_code}")
