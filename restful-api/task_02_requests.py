63c8f8d2abaa71d3c51c9986b178c6e3f2882dfe
Bryanjose001 <bryan.jose2541@gmail.com>
2025-06-15 15:16:45 -0400

    post.csv

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,7 +6,7 @@ from http import HTTPStatus
 
 def fetch_and_print_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print ('status Code:',r.status_code)
+    print ('Status Code:',r.status_code)
     if r.status_code == HTTPStatus.OK:
         data = r.json()  
         if data: 
@@ -16,16 +16,51 @@ def fetch_and_save_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
     if r.status_code == HTTPStatus.OK:
         data = r.json()
-        structured_posts = [{'id': post['id'], 'title': post['title'],
-                            'body': post['body']} for post in posts]
+        # Check if Data is not empty
+        if data:
+            # Iterate through data
+            post_list = []
+            for post in data:
+                # For each row of data, get the id, title and body
+                id = post['id']
+                title = post['title']
+                body = post['body']
+                # Store the information inside a dictionary
+                post_dict = {
+                    'id': id,
+                    'title': title,
+                    'body': body
+                }
+                # Add the dictionary to a list
+                post_list.append(post_dict) 
+            # Define output file path
+            filename = "post.csv"
 
-        with open('posts.csv', mode='w', newline='',
-                  encoding='utf-8') as csv_file:
-            fieldnames = ['id', 'title', 'body']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            # Get the fieldnames (CSV column headers)
+            fieldnames = post_list[0].keys()
 
-            writer.writeheader()
-            writer.writerows(structured_posts)
-        print("Data has benn succesfully written to posts.csv")
-    else:
-        print(f"Failed to fetch posts. Status code: {r.status_code}")
+            # Write to CSV
+            with open(filename, mode="w", newline="", encoding="utf-8") as f:
+                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                writer.writeheader()  # Write column names
+                writer.writerows(post_list)  # Write all rows
+    
+
+
+    # use list of dictionaries to print contents inside a CSV file
+      
+    #     structured_posts = [{'id': post['id'], 'title': post['title'],
+    #                         'body': post['body']} for post in posts]
+
+    #     with open('posts.csv', mode='w', newline='',
+    #               encoding='utf-8') as csv_file:
+    #         fieldnames = ['id', 'title', 'body']
+    #         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    #         writer.writeheader()
+    #         writer.writerows(structured_posts)
+    #     print("Data has benn succesfully written to posts.csv")
+    # else:
+    #     print(f"Failed to fetch posts. Status code: {r.status_code}")
+
+fetch_and_save_posts()
