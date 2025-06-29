f9e96774c3d851e836e663f91ee5da4fd3ec97ac
Bryanjose001 <bryan.jose2541@gmail.com>
2025-06-29 14:54:45 -0400

    obj

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""This script connects to a MySQL database and lists all states"""
+"""Python - Object Relational Mapping ft MySQLdb and sys"""
 import MySQLdb
 import sys
 
 
-def list_states(username, password, database_name):
-    """Connects to MySQL server and lists
-    all states in ascending order by id"""
-    try:
-        conn = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database_name
-        )
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT * FROM states ORDER BY id ASC")
-            for state in cursor.fetchall():
-                print(state)
-
-        conn.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL or executing query: {e}")
-
-
 if __name__ == "__main__":
-    # Get command-line arguments and call the function
-    if len(sys.argv) != 4:
-        print("Usage: ./script.py <username> <password> <database_name>")
-        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    list_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+        )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
