bf4d7501b64eba468e3ed76407e3f15571c8467a
bryanjosepr <bryan.jose2541@gmail.com>
2025-07-19 19:31:50 -0400

    java semi fix

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // This script adds a new item to the list when the button is clicked
+  // This script adds a new item to the list when the button is clicked
   const item = document.getElementById('add_item');
   const list = document.querySelector('.my_list');
 
